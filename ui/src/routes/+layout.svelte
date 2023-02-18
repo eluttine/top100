2f2c1d36f7380d38be2428defd0cebf552f30ca2
Elias Luttinen <elias.luttinen@gmail.com>
2023-02-18 08:42:25 +0200

    UI folder

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -2,4 +2,20 @@
 	import '../app.postcss';
 </script>
 
-<slot />
+<div class="bg-neural">
+	<div class="max-w-xl mx-auto navbar">
+		<div class="navbar-start">
+			<a href="/" class="btn btn-ghost text-xl">Top 100 Ruokablogit</a>
+		</div>
+		<div class="navbar-end">
+			<ul class="menu menu-horizontal">
+				<li><a href="/login">Kirjaudu</a></li>
+				<li><a href="/register">Rekisteröidy</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="max-w-xl mx-auto py-8 px-4">
+	<slot />
+</div>
---
+++ ui/src/routes/register/+page.svelte
@@ -0,0 +1,14 @@
+<form action="POST" class="card">
+	<h1 class="text-2xl mb-8">Rekisteröidy</h1>
+	<div class="form-control gap-2 mb-2">
+		<input type="email" name="email" placeholder="Sähköposti" class="input input-bordered" />
+		<input type="password" name="password" placeholder="Salasana" class="input input-bordered" />
+		<input
+			type="password"
+			name="passwordConfirm"
+			placeholder="Salasanan varmistus"
+			class="input input-bordered"
+		/>
+		<button class="btn btn-primary">Rekisteröidy</button>
+	</div>
+</form>
